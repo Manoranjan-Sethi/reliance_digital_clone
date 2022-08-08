103bcba4f41232d0d69ffe1d9b104a648324f0f7
krishnachaturvedi1901 <krishna.chaturvedi1901@gmail.com>
2022-08-08 17:32:31 +0530

    reCommit

--- Pages/cart.js
+++
@@ -1,8 +0,0 @@
-import {navbar} from "../component/navbar.js "
-import {footer} from "../component/footer.js"
-
-let navMain=document.getElementById("navMain")
-navMain.innerHTML=navbar()
-
-let footerMain=document.getElementById("footerMain")
-footerMain.innerHTML=footer()
